7092d6d7fc8e4c07bb7ebde4bc38aa5b5fc25ac3
Jinyshin <87403267+Jjinyshin@users.noreply.github.com>
2024-10-02 21:30:19 +0900

    [level 1] Title: K번째수, Time: 0.58 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int index = 0; index < commands.length; index++) {
+            int i = commands[index][0];
+            int j = commands[index][1];
+            int k = commands[index][2];
+            
+            int[] subArray = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(subArray);
+            
+            answer[index] = subArray[k-1];
+        }   
+        return answer;
+    }
+}
