b28f97019ea124de7b085219438021566320d7eb
Jinyshin <87403267+Jjinyshin@users.noreply.github.com>
2024-10-03 15:15:39 +0900

    [level 2] Title: 가장 큰 수, Time: 293.88 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 403 MB, 시간: 647.91 ms
+메모리: 159 MB, 시간: 293.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 03일 14:45:07
+2024년 10월 03일 15:15:40
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,76 +1,33 @@
-import java.util.Arrays;
+import java.util.*;
 
 /*
-- 숫자들을 합치고 크기 비교를 하기 위해 숫자 배열을 문자열 배열로 바꿈
-- 퀵소트 수행
-- 정렬된 결과를 이어 붙여서 최종적인 결과를 반환한다.
-- 모든 숫자들이 0인 경우를 예외처리하기
+다른 사람의 풀이 참고
 */
 
-public class Solution {
-    
+public class Solution {    
     public static String solution(int[] numbers) {
-        // 숫자 배열을 문자열 배열로 변환
-        String[] strNumbers = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            strNumbers[i] = String.valueOf(numbers[i]);
+        String result = "";
+        
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < numbers.length; i++) {
+            list.add(numbers[i]);
         }
         
-        quickSort(strNumbers, 0, strNumbers.length - 1);
-        
-        StringBuilder result = new StringBuilder();
-        for (String num : strNumbers) {
-            result.append(num);
+        Collections.sort(list, (a, b) -> {
+            String as = String.valueOf(a), bs = String.valueOf(b);
+            return -Integer.compare(Integer.parseInt(as + bs), Integer.parseInt(bs + as));
+        });
+                
+        StringBuilder sb = new StringBuilder();
+        for (Integer i : list) {
+            sb.append(i);
         }
         
-        // "000..." -> "0" 반환
-        if (result.toString().startsWith("0")) {
+        result = sb.toString();
+        if (result.startsWith("0")) {
             return "0";
+        } else {
+            return result;
         }
-        
-        return result.toString();
-    }
-    
-    private static void quickSort(String[] arr, int left, int right) {
-        if (left < right) {
-            int pivotIndex = partition(arr, left, right);
-            quickSort(arr, left, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, right);
-        }
-    }
-    
-    // 피벗 기준으로 합친 문자열의 크기 비교(ab VS ba)
-    private static int partition(String[] arr, int left, int right) {
-        String pivot = arr[right];
-        int i = left;
-        int j = right - 1;
-
-        while (i <= j) {
-            // i는 피벗보다 작은 값을 찾을 때까지 오른쪽으로 이동
-            while ((arr[i] + pivot).compareTo(pivot + arr[i]) > 0) {
-                i++;
-            }
-
-            // j는 피벗보다 큰 값을 찾을 때까지 왼쪽으로 이동
-            while (i <= j && (arr[j] + pivot).compareTo(pivot + arr[j]) <= 0) {
-                j--;
-            }
-
-            // 엇갈리지 않았다면 교환
-            if (i < j) {
-                swap(arr, i, j);
-            }
-        }
-
-        // 피벗을 i 위치로 교환하여 피벗이 올바른 위치에 오게 함
-        swap(arr, i, right);
-        return i;
-    }
-
-    
-    private static void swap(String[] arr, int i, int j) {
-        String temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
