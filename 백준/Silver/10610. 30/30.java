f735771d0391a94025ff7e58c9df5f315efb3f27
Jinyoung Shin <87403267+Jinyshin@users.noreply.github.com>
2024-12-03 18:58:33 +0900

    [Silver IV] Title: 30, Time: 308 ms, Memory: 22536 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,3 +1,17 @@
+/*
+양수 N
+N에 있는 숫자들을 섞어서 30의 배수가 되는 가장 큰 수를 만들어라
+
+N은 최대 10^5개의 숫자로 구성되어있고, 0으로 시작하지 않음.
+존재x -> -1출력
+    N이 n자리 수라고 했을 때, 모든 경우의 수는 n!, 최대 10^5 = 10^5! -> 따라서 완전탐색으로는 불가능
+
+30의 배수의 특징?
+    30 = 2 * 3 * 5
+    - 0으로 끝나야 함. -> 2,5의 배수
+    - 자릿수의 합이 3의 배수여야 함. -> 3의 배수
+ */
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -16,25 +30,19 @@ public class Main {
     }
 
     static boolean canBeThirtyTimes(String number) {
+        char[] nums = number.toCharArray();
         int sum = 0;
-        for (int i = 0; i < number.length(); i++) {
-            sum += ((int) number.charAt(i));
+        for (char num : nums) {
+            sum += num - '0';
         }
-        // 30의 배수가 되는 수를 만들 수 없으면
+        // 30의 배수가 되는 수를 만들 수 없으면 false
         if (!number.contains("0") || sum % 3 != 0) {
             return false;
         }
-
-        int[] numbers = new int[number.length()];
-        for (int i = 0; i < number.length(); i++) {
-            numbers[i] = number.charAt(i) - '0';
-        }
-        Arrays.sort(numbers);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < numbers.length; i++) {
-            sb.append(numbers[numbers.length - 1 - i]);
-        }
-        biggestNumber = sb.toString();
+        // 가능한 가장 큰 30의 배수 찾기
+        Arrays.sort(nums);
+        StringBuilder sb = new StringBuilder(new String(nums));
+        biggestNumber = sb.reverse().toString();
 
         return true;
     }
