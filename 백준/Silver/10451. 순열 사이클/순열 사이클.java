9ff1e2ee0ed8831b2bc0796d255b14750e0b5d0e
Jinyoung Shin <87403267+Jinyshin@users.noreply.github.com>
2024-12-30 15:46:40 +0900

    [Silver III] Title: 순열 사이클, Time: 1172 ms, Memory: 310964 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.java
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
 public class Main {
-        static int T, N;
+    static int T, N;
     static int[] visited;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         T = sc.nextInt();
-        StringBuilder result = new StringBuilder();
         for (int t = 0; t < T; t++) {
             N = sc.nextInt();
             visited = new int[N + 1];
@@ -14,9 +14,8 @@ public class Main {
             for (int i = 1; i <= N; i++) {
                 permutation[i] = sc.nextInt();
             }
-            result.append(countCycle(permutation)).append("\n");
+            System.out.println(countCycle(permutation));
         }
-        System.out.print(result);
     }
 
     public static int countCycle(int[] permutation) {
