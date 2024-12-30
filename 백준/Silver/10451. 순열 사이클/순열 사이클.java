43c63348c0af1196c809b5f0b598d05b024cbf1e
Jinyoung Shin <87403267+Jinyshin@users.noreply.github.com>
2024-12-30 15:41:27 +0900

    [Silver III] Title: 순열 사이클, Time: 1200 ms, Memory: 309900 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.java
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -1,37 +1,37 @@
 import java.util.*;
 
 public class Main {
-    static int T, N;
+        static int T, N;
     static int[] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         T = sc.nextInt();
+        StringBuilder result = new StringBuilder();
         for (int t = 0; t < T; t++) {
             N = sc.nextInt();
             visited = new int[N + 1];
-            int[] permutation = new int[N + 1]; 
+            int[] permutation = new int[N + 1];
             for (int i = 1; i <= N; i++) {
                 permutation[i] = sc.nextInt();
             }
-            countCycle(permutation);
+            result.append(countCycle(permutation)).append("\n");
         }
+        System.out.print(result);
     }
 
-    public static void countCycle(int[] permutation) {
-        ArrayList<ArrayList<Integer>> pCycleList = new ArrayList<>();
+    public static int countCycle(int[] permutation) {
+        int cycleCount = 0;
         for (int i = 1; i <= N; i++) {
-            ArrayList<Integer> graph = new ArrayList<>();
-            int key = i;
-            if (visited[key] == 1) {
+            if (visited[i] == 1) {
                 continue;
             }
-            while (!graph.contains(key)) {
-                graph.add(key);
-                key = permutation[key];
+            int key = i;
+            while (visited[key] == 0) {
                 visited[key] = 1;
+                key = permutation[key];
             }
-            pCycleList.add(graph);
+            cycleCount++;
         }
-        System.out.println(pCycleList.size());
+        return cycleCount;
     }
 }
