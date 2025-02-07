cb565fbe2256e4ece051dbe2ac5cad204477473d
Jinyoung Shin <87403267+Jinyshin@users.noreply.github.com>
2025-02-07 23:24:25 +0900

    [level 2] Title: 전화번호 목록, Time: 284.29 ms, Memory: 247 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,13 +2,26 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        HashMap<String, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
+        for (String number : phone_book) {
+            map.put(number, 1);
+        }
+        
+        for (String number : phone_book) {
+            for (int i = 1; i < number.length(); i++) {
+                String prefix = number.substring(0, i);
+                if (map.containsKey(prefix)) return false;
             }
         }
         return true;
     }
 }
+
+/*
+해시맵 활용해서 풀기
+
+각 전화번호부를 해시맵에 저장
+각 전화번호로 만들 수 있는 모든 접두어들이 해시맵에 존재하는지 확인
+    1235 -> 1, 12, 123 확인 -> 존재하면 false
+*/
