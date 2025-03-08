b2df68218b67ce915d13d9b15e43714dc5bfe2f6
Jinyoung Shin <87403267+Jinyshin@users.noreply.github.com>
2025-03-09 01:57:18 +0900

    [level 1] Title: 모의고사, Time: 3.50 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -10,32 +10,43 @@ class Solution {
     public int[] solution(int[] answers) {
         int[] corrects = new int[3];
         int max = 0;
+        List<Integer> topScorers = new ArrayList<>();
         
         // 각 수포자별로 정답 개수 세기
         for (int i = 0; i < answers.length; i++) {
             int answer = answers[i];
             for (int person = 0; person < 3; person++) {
-                int supoAnswer = getSupoAnswer(person, i);
-                if (answer == supoAnswer) {
-                    if (++corrects[person] > max) max = corrects[person];
+                if (answer == RULES[person][i % RULES[person].length]) {
+                    corrects[person]++;
                 }
             }
         }
         
-        final int finalMax = max;
-        return IntStream.range(0, 3)
-            .filter(i -> corrects[i] == finalMax)
-            .map(i -> i + 1)
-            .toArray();
-    }
-    
-    private int getSupoAnswer(int person, int problem) {
-        int[] rule = RULES[person];
-        int index = problem % rule.length;
-        return rule[index];
+        max = Arrays.stream(corrects).max().getAsInt();
+        // max = Math.max(corrects[0], Math.max(corrects[1], corrects[2]));
+        
+        for (int i = 0; i < 3; i++) {
+            if (corrects[i] == max) {
+                topScorers.add(i + 1);
+            }
+        }
+        
+        return topScorers.stream().mapToInt(Integer::intValue).toArray();
+        
+        // final int finalMax = max;
+        // return IntStream.range(0, 3)
+        //     .filter(i -> corrects[i] == finalMax)
+        //     .map(i -> i + 1)
+        //     .toArray();
     }
 }
 
 /*
 각 수포자별로 정답수 세는 횟수 <= 3 * 10^4 -> 1초 가능
+
+--
+최적화
+
+- RULES[person][i % RULES[person].length] 를 사용하여 불필요한 메서드 제거
+- 최댓값을 위한 변수를 2개씩 쓰지 않고, 반복문 로직 이후 한번에 구하도록 변경
 */
