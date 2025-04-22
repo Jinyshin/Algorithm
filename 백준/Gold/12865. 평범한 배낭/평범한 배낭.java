9e8df66195693db7971cf1b4cd7f4682f45e0f48
Jinyoung Shin <87403267+Jinyshin@users.noreply.github.com>
2025-04-22 23:19:24 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53624 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -7,29 +7,26 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[][] wv = new int[N+1][2];
-
+        int[][] wv = new int[N + 1][2];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            int W = Integer.parseInt(st.nextToken());
-            int V = Integer.parseInt(st.nextToken());
-            wv[i][0] = V;
-            wv[i][1] = W;
+            wv[i][0] = Integer.parseInt(st.nextToken());
+            wv[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        int[][] mv = new int[N+1][K+1];
+        
+        int[][] maxV = new int[N + 1][K + 1];
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= K; j++) {
-                int num1 = mv[i-1][j];
+                int num1 = maxV[i - 1][j];
                 int num2 = 0;
-                if (j - wv[i][1] >= 0) {
-                    num2 = mv[i-1][j - wv[i][1]] + wv[i][0];
+                if (j >= wv[i][0]) {
+                    num2 = wv[i][1] + maxV[i - 1][j - wv[i][0]];
                 }
-                mv[i][j] = Math.max(num1, num2);
+                maxV[i][j] = Math.max(num1, num2);
             }
         }
 
-        System.out.println(mv[N][K]);
+        System.out.println(maxV[N][K]);
     }
 }
